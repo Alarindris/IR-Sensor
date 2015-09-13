6b97ca9b517e4bacad0e8922fe45d8a8dd160239
Alarindris <erik.sikich@gmail.com>
2015-09-13 18:40:45 -0500

    Rewrite, condensed code.  Removed old libraries.

--- IRSensor/IRSensor.ino
+++ IRSensor/IRSensor.ino
@@ -6,40 +6,24 @@
 #define RELAY_OFFSET 50
 #define ON_DELAY 1000
 #define OFF_DELAY 500
+#define INTERVAL 25
 
 Bounce debouncer = Bounce(); 
 
 void setup() {
-  pinMode(BUTTON_PIN,INPUT_PULLUP);
-  debouncer.attach(BUTTON_PIN);
-  debouncer.interval(25); // interval in ms
-  pinMode(ION_PIN, OUTPUT);
-  pinMode(AIR_PIN, OUTPUT);
-
+    pinMode(BUTTON_PIN,INPUT_PULLUP);
+    debouncer.attach(BUTTON_PIN);
+    debouncer.interval(INTERVAL);
+    pinMode(ION_PIN, OUTPUT);
+    pinMode(AIR_PIN, OUTPUT);
 }
  
 void loop() {
-  int value;
-  int prevVal;
-  
-  while(true){
-    debouncer.update();
-    value = debouncer.read();
-    if(value != prevVal){
-      if ( value == LOW ) {
-        digitalWrite(AIR_PIN, HIGH);
+    if(debouncer.update()){
+        bool value = debouncer.read();
+        digitalWrite(AIR_PIN, !value);
         delay(RELAY_OFFSET);
-        digitalWrite(ION_PIN, HIGH);
+        digitalWrite(ION_PIN, !value);
         delay(ON_DELAY - RELAY_OFFSET);
-      } 
-      else {
-        digitalWrite(ION_PIN, LOW);
-        delay(RELAY_OFFSET);
-        digitalWrite(AIR_PIN, LOW);
-        delay(OFF_DELAY - RELAY_OFFSET);
-      }
     }
-    prevVal = value;
-  }
 }
-
