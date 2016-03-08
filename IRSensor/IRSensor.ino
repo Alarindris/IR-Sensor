4cc83f83f71743540491504f9e8c3481af59ad85
Alarindris <erik.sikich@gmail.com>
2016-03-08 08:45:13 -0600

    Removed old debouncing stuff.

--- IRSensor/IRSensor.ino
+++ IRSensor/IRSensor.ino
@@ -1,13 +1,8 @@
 #define BUTTON_PIN 2
 #define ION_PIN 11
 #define AIR_PIN 12
-#define RELAY_OFFSET 50
-#define ON_DELAY 50
-#define OFF_DELAY 25
-#define INTERVAL 25
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(ION_PIN, OUTPUT);
   pinMode(AIR_PIN, OUTPUT);
