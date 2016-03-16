ec36d81ee8069b8727a2a3577fb65886ee488459
Alarindris <erik.sikich@gmail.com>
2016-03-16 08:02:24 -0500

    Added comments.

--- IRSensor/IRSensor.ino
+++ IRSensor/IRSensor.ino
@@ -2,6 +2,8 @@
 #define ION_PIN 11
 #define AIR_PIN 12
 
+//Switch connects pin 2 to ground.
+
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(ION_PIN, OUTPUT);
@@ -9,9 +11,11 @@ void setup() {
 }
 
 void loop() {
+  //switch on
   if(digitalRead(BUTTON_PIN)){
     digitalWrite(ION_PIN, HIGH);
     digitalWrite(AIR_PIN, HIGH);
+  //switch off
   }else{
     digitalWrite(ION_PIN, LOW);
     digitalWrite(AIR_PIN, LOW);
